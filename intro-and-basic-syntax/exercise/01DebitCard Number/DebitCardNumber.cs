71fad73f037842fe1a2846bf96b5c94035a0590c
Bebbo <b.victoroff@abv.bg>
2018-03-07 20:36:02 +0200

    fix folder

---
+++ intro-and-basic-syntax/exercise/01DebitCard Number/DebitCardNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01DebitCard_Number
+{
+    class DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            int FirstNumber = int.Parse(Console.ReadLine());
+            int SecondNumber = int.Parse(Console.ReadLine());
+            int ThirdNumber = int.Parse(Console.ReadLine());
+            int FourthNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0:D4} {1:d4} {2:D4} {3:D4} ", FirstNumber, SecondNumber, ThirdNumber, FourthNumber);
+        }
+    }
+}
