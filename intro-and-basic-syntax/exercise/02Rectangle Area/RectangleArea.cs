0bf68910619ea61281c3f890820afcc898e8fda6
Angel Stoykov <angelsto@abv.bg>
2018-03-07 21:05:34 +0200

    Added 02Rectangle Area

---
+++ intro-and-basic-syntax/exercise/02Rectangle Area/RectangleArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02Rectangle_Area
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            double Width = double.Parse(Console.ReadLine());
+            double Height = double.Parse(Console.ReadLine());
+
+            double Result = Width * Height;
+            Console.WriteLine("{0:F2} ", Result);
+        }
+    }
+}
