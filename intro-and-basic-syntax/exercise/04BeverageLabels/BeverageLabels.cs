cd48419f92f362168cb74e84df8129ec9340938f
Angel Stoykov <angelsto@abv.bg>
2018-03-07 21:06:12 +0200

    Added 04BeverageLabels

---
+++ intro-and-basic-syntax/exercise/04BeverageLabels/BeverageLabels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04BeverageLabels
+{
+    class BeverageLabels
+    {
+        static void Main(string[] args)
+        {
+            var Name = Console.ReadLine();
+            var Volume = int.Parse(Console.ReadLine());
+            var EnergyCont = int.Parse(Console.ReadLine());
+            var SugarCont = int.Parse(Console.ReadLine());
+
+            var Calories = Volume * EnergyCont / 100.0;
+            var Sugar = Volume * SugarCont / 100.0;
+
+            Console.WriteLine("{0}ml {1}:", Volume, Name);
+            Console.WriteLine("{0}kcal, {1}g sugars", Calories, Sugar);
+
+        }
+    }
+}
