b381f7e533aa6ee26bd742ff3041527f95b2575f
rymen96 <35739430+rymen96@users.noreply.github.com>
2018-03-07 20:36:31 +0200

    Added exercise 03MileТoKilometers

---
+++ 03MileТoKilometers/MileТoKilometers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03MileТoKilometers
+{
+    class MileТoKilometers
+    {
+        static void Main(string[] args)
+        {
+            double Miles = double.Parse(Console.ReadLine());
+            double KmPerMile = 1.60934;
+            double Kilometres = Miles * (double)KmPerMile;
+            Console.WriteLine("{0:F2}", Kilometres);
+        }
+    }
+}
