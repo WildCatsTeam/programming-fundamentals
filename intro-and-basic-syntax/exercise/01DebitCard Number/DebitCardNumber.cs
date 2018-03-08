938ed65b78d5e9783ae237c24df85f87be3c6adc
rymen96 <35739430+rymen96@users.noreply.github.com>
2018-03-09 00:22:40 +0200

    Added comment to code
    Signed-off-by: rymen96 <35739430+rymen96@users.noreply.github.com>

--- intro-and-basic-syntax/exercise/01DebitCard Number/DebitCardNumber.cs
+++ intro-and-basic-syntax/exercise/01DebitCard Number/DebitCardNumber.cs
@@ -6,10 +6,13 @@ namespace _01DebitCard_Number
     {
         static void Main(string[] args)
         {
+            // read from console 4 lines with four number per line
             int FirstNumber = int.Parse(Console.ReadLine());
             int SecondNumber = int.Parse(Console.ReadLine());
             int ThirdNumber = int.Parse(Console.ReadLine());
             int FourthNumber = int.Parse(Console.ReadLine());
+
+            // Adding all number in 1 line and print
             Console.WriteLine("{0:D4} {1:d4} {2:D4} {3:D4} ", FirstNumber, SecondNumber, ThirdNumber, FourthNumber);
         }
     }
