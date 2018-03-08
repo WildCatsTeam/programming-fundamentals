bbd96274129a0f57bf3a65b3d8555abcfc7981b7
rymen96 <35739430+rymen96@users.noreply.github.com>
2018-03-09 00:11:07 +0200

    Added comment to code

--- intro-and-basic-syntax/exercise/04BeverageLabels/BeverageLabels.cs
+++ intro-and-basic-syntax/exercise/04BeverageLabels/BeverageLabels.cs
@@ -6,14 +6,16 @@ namespace _04BeverageLabels
     {
         static void Main(string[] args)
         {
+            // We read from console Name, Volume, EnergyCount and SugarCount
             var Name = Console.ReadLine();
             var Volume = int.Parse(Console.ReadLine());
             var EnergyCont = int.Parse(Console.ReadLine());
             var SugarCont = int.Parse(Console.ReadLine());
-
+            // Here we calculate Calories per 100gr
             var Calories = Volume * EnergyCont / 100.0;
+            // Here calculate Sugar per 100gr
             var Sugar = Volume * SugarCont / 100.0;
-
+            //Write results
             Console.WriteLine("{0}ml {1}:", Volume, Name);
             Console.WriteLine("{0}kcal, {1}g sugars", Calories, Sugar);
 
