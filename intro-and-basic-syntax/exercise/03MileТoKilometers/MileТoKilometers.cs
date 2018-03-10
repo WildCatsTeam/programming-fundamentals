647f7ed46a6183cc1d949c226f008d51a9b86755
Trinity7509 <iren75iv@gmail.com>
2018-03-10 08:56:47 +0200

    Adding comments to third task

--- intro-and-basic-syntax/exercise/03MileТoKilometers/MileТoKilometers.cs
+++ intro-and-basic-syntax/exercise/03MileТoKilometers/MileТoKilometers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _03MileТoKilometers
 {
@@ -6,9 +6,11 @@ namespace _03MileТoKilometers
     {
         static void Main(string[] args)
         {
+		//we are reading from console Miles and Kilometers perMile
             double Miles = double.Parse(Console.ReadLine());
             double KmPerMile = 1.60934;
             double Kilometres = Miles * (double)KmPerMile;
+			//giving a formula to calculate the result and getting the result
             Console.WriteLine("{0:F2}", Kilometres);
         }
     }
