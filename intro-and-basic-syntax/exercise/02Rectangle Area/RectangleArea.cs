e0f5b392c6bff510a9adfc5fce710746e0b1b354
Angel Stoykov <angelsto@abv.bg>
2018-03-11 18:35:11 +0200

    Added comments on tasks 02.Rectangle Area and 05.Character Stats. Added team.txt

--- intro-and-basic-syntax/exercise/02Rectangle Area/RectangleArea.cs
+++ intro-and-basic-syntax/exercise/02Rectangle Area/RectangleArea.cs
@@ -6,10 +6,14 @@ namespace _02Rectangle_Area
     {
         static void Main(string[] args)
         {
+            // Here we receive the input data - width and height of the rectangle.
             double Width = double.Parse(Console.ReadLine());
             double Height = double.Parse(Console.ReadLine());
 
+            // Calculate the result (area).
             double Result = Width * Height;
+
+            // Printing the result at the console.
             Console.WriteLine("{0:F2} ", Result);
         }
     }
