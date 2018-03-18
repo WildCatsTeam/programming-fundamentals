677327a129fb53653c463decd66f3c3516e5622f
rymen96 <35739430+rymen96@users.noreply.github.com>
2018-03-18 16:37:35 +0200

    change variable name:Rymen96

--- intro-and-basic-syntax/exercise/03MileТoKilometers/MileТoKilometers.cs
+++ intro-and-basic-syntax/exercise/03MileТoKilometers/MileТoKilometers.cs
@@ -8,10 +8,10 @@ namespace _03MileТoKilometers
         {
 		//we are reading from console Miles and Kilometers perMile
             double Miles = double.Parse(Console.ReadLine());
-            double KmPerMile = 1.60934;
-            double Kilometres = Miles * (double)KmPerMile;
+            double MilesPerKm = 1.60934;
+            double total = Miles * (double)KmPerMile;
 			//giving a formula to calculate the result and getting the result
-            Console.WriteLine("{0:F2}", Kilometres);
+            Console.WriteLine("{0:F2}", total);
         }
     }
 }
